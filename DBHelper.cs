b991f3054deeba3ba9f0f23ad1f079a5c350db7d
hikl <28390@DESKTOP-JEPIKEM>
2022-05-25 14:16:22 +0800

    添加项目文件。

---
+++ DBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Kojiro_ordering_management_system
+{
+    internal class DBHelper
+    {
+        public static string CoonString = "server=.;database=Kojiror;uid=sa;pwd=1234";
+        public static SqlConnection conn = null; 
+        public static void connt()
+        {
+            if (conn == null)
+            {
+                conn = new SqlConnection(CoonString);
+            }
+            if (conn.State==ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            if (conn.State==ConnectionState.Broken)
+            {
+                conn.Close();
+                conn.Open();
+            }
+        }
+        public static SqlDataReader GDR(string sql)
+        {
+            connt();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+        public static bool ENQ(string sql)
+        {
+            connt();
+            SqlCommand cmd=new SqlCommand(sql, conn);
+            int result= cmd.ExecuteNonQuery();
+            conn.Close();
+            return result>0;
+        }
+        public static object ES(string sql)
+        {
+            connt();
+            SqlCommand cmd= new SqlCommand(sql, conn);
+            object result= cmd.ExecuteScalar();
+            conn.Close();
+            return result;
+        }
+    }
+}
