ded72551e838527bd801879c05fff247d570d59d
BuXianWanYin <105205569+BuXianWanYin@users.noreply.github.com>
2022-05-27 12:50:34 +0800

    优化

--- enroll.Designer.cs
+++ enroll.Designer.cs
@@ -59,6 +59,8 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -70,45 +72,46 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox2.Location = new System.Drawing.Point(366, 91);
+            this.textBox2.Location = new System.Drawing.Point(366, 93);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(224, 31);
+            this.textBox2.Size = new System.Drawing.Size(224, 28);
             this.textBox2.TabIndex = 12;
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("新宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(366, 45);
+            this.textBox1.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(366, 47);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 31);
+            this.textBox1.Size = new System.Drawing.Size(224, 28);
             this.textBox1.TabIndex = 11;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(365, 232);
+            this.textBox5.Location = new System.Drawing.Point(365, 231);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(224, 31);
+            this.textBox5.Size = new System.Drawing.Size(224, 28);
             this.textBox5.TabIndex = 15;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox3.Location = new System.Drawing.Point(365, 140);
+            this.textBox3.Location = new System.Drawing.Point(365, 141);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.PasswordChar = '*';
-            this.textBox3.Size = new System.Drawing.Size(224, 31);
+            this.textBox3.Size = new System.Drawing.Size(224, 28);
             this.textBox3.TabIndex = 19;
             // 
             // label1
@@ -155,10 +158,10 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox4.Location = new System.Drawing.Point(366, 186);
+            this.textBox4.Location = new System.Drawing.Point(366, 188);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(224, 31);
+            this.textBox4.Size = new System.Drawing.Size(224, 28);
             this.textBox4.TabIndex = 25;
             // 
             // label5
@@ -183,11 +186,11 @@
             // 
             // textBox6
             // 
-            this.textBox6.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox6.Location = new System.Drawing.Point(362, 282);
+            this.textBox6.Font = new System.Drawing.Font("华文细黑", 12F);
+            this.textBox6.Location = new System.Drawing.Point(366, 284);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(228, 31);
+            this.textBox6.Size = new System.Drawing.Size(224, 28);
             this.textBox6.TabIndex = 28;
             // 
             // linkLabel2
@@ -215,10 +218,10 @@
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox7.Location = new System.Drawing.Point(362, 328);
+            this.textBox7.Location = new System.Drawing.Point(366, 334);
             this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(55, 31);
+            this.textBox7.Size = new System.Drawing.Size(91, 28);
             this.textBox7.TabIndex = 36;
             // 
             // statusStrip1
@@ -261,12 +264,13 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Kojiro_ordering_management_system.Properties.Resources.刷新;
-            this.pictureBox1.Location = new System.Drawing.Point(550, 328);
+            this.pictureBox1.Location = new System.Drawing.Point(539, 337);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(39, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(39, 29);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 34;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox9
             // 
@@ -348,12 +352,33 @@
             this.pictureBox5.TabIndex = 18;
             this.pictureBox5.TabStop = false;
             // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(463, 335);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(66, 28);
+            this.pictureBox11.TabIndex = 39;
+            this.pictureBox11.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(362, 369);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 12);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "验证码错误！";
+            this.label8.Visible = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(625, 503);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.pictureBox10);
@@ -400,6 +425,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -435,5 +461,7 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Label label8;
     }
 }
--- enroll.cs
+++ enroll.cs
@@ -34,7 +34,13 @@ namespace Kojiro_ordering_management_system
             timer1.Interval = 1000;//1秒
             this.timer1.Start();//开启计时器
 
+            //加载时生成验证码
+            code = GenerateCheckCode();//生成4位数字符串
+            Bitmap image = CreateCheckCodeImage(code, 64, 30);//生成图片
+            pictureBox11.Image = image;//给控件赋值
+
         }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             
@@ -52,12 +58,164 @@ namespace Kojiro_ordering_management_system
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox7.Text))//判断验证码框是否为空
+            {
+                MessageBox.Show("请输入验证码");
+                return;
+            }
+            if (textBox7.Text.Equals(code))
+            {
+                MessageBox.Show("成功");
+                label8.Visible = false;
+            }
+            else
+            {
+                label8.Visible = true;//验证码错误时 显示错误文本
+                textBox7.Text = "";//清空文本框
+                //然后刷新验证码
+                code = GenerateCheckCode();//生成4位数字符串
+                Bitmap image = CreateCheckCodeImage(code, 64, 30);//生成图片
+                pictureBox11.Image = image;//给控件赋值
 
+                textBox7.GotFocus += new EventHandler((obj, ex) => { label8.Visible = false; });//成为焦点时把错误文本隐藏
+                }
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        //验证码
+        /// <summary>
+        /// 得到验证码
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private string GenerateCheckCode()
+        {
+            int number;
+            char code;
+            string checkCode = String.Empty;
+
+            System.Random random = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                number = random.Next();
+
+                if (number % 2 == 0)
+                    code = (char)('0' + (char)(number % 10));
+                else if (number % 3 == 0)
+                    code = (char)('A' + (char)(number % 26));
+                else
+                    code = (char)('a' + (char)(number % 26));
+                checkCode += code.ToString();
+            }
+            return checkCode.ToUpper();
+        }
+
+        /// <summary>
+        /// 生成验证码图片
+        /// </summary>
+        /// <param name="checkCode"></param>
+        /// <param name="context"></param>
+        private Bitmap CreateCheckCodeImage(string checkCode, int w = 54, int h = 30)
+        {
+            System.Drawing.Bitmap image = new System.Drawing.Bitmap(w, h);
+            Graphics g = Graphics.FromImage(image);
+
+            g.Clear(Color.WhiteSmoke);//清除背景色
+
+            Color[] c = { Color.Black, Color.Red, Color.DarkBlue, Color.Green, Color.Orange, Color.Brown, Color.DarkCyan, Color.Purple };//定义随机颜色
+
+            string[] font = { "Verdana", "Microsoft Sans Serif", "Comic Sans MS", "Arial", "宋体" };
+            Random rand = new Random();
+            int z = 0;//干扰线条数
+            for (int i = 0; i < z; i++)
+            {
+                int x1 = rand.Next(image.Width);
+                int x2 = rand.Next(image.Width);
+                int y1 = rand.Next(image.Height);
+                int y2 = rand.Next(image.Height);
+                g.DrawLine(new Pen(Color.LightGray, 1), x1, y1, x2, y2);//根据坐标画线
+            }
+
+            for (int i = 0; i < checkCode.Length; i++)
+            {
+                int cindex = rand.Next(7);
+                int findex = rand.Next(5);
+
+                Font f = new System.Drawing.Font(font[findex], 14, System.Drawing.FontStyle.Bold);
+                Brush b = new System.Drawing.SolidBrush(c[cindex]);
+                int ii = 4;
+                if ((i + 1) % 2 == 0)
+                {
+                    ii = 2;
+                }
+                g.DrawString(checkCode.Substring(i, 1), f, b, 3 + (i * (14)), ii);
+            }
+            image = TwistImage(image, true, 5, 1);
+            g.DrawRectangle(new Pen(Color.Black, 0), 0, 0, image.Width - 1, image.Height - 1);
+            return image;
+        }
+        
+
+        private const double PI = 3.1415926535897932384626433832795;
+        private const double PI2 = 6.283185307179586476925286766559;
+
+        /// <summary>
+        /// 正弦曲线Wave扭曲图片（Edit By 51aspx.com）
+        /// </summary>
+        /// <param name="srcBmp">图片路径</param>
+        /// <param name="bXDir">如果扭曲则选择为True</param>
+        /// <param name="nMultValue">波形的幅度倍数，越大扭曲的程度越高，一般为3</param>
+        /// <param name="dPhase">波形的起始相位，取值区间[0-2*PI)</param>
+        /// <returns></returns>
+        public System.Drawing.Bitmap TwistImage(Bitmap srcBmp, bool bXDir, double dMultValue, double dPhase)
+        {
+            System.Drawing.Bitmap destBmp = new Bitmap(srcBmp.Width, srcBmp.Height);
+
+            // 将位图背景填充为白色
+            System.Drawing.Graphics graph = System.Drawing.Graphics.FromImage(destBmp);
+            graph.FillRectangle(new SolidBrush(System.Drawing.Color.White), 0, 0, destBmp.Width, destBmp.Height);
+            graph.Dispose();
+
+            double dBaseAxisLen = bXDir ? (double)destBmp.Height : (double)destBmp.Width;
+
+            for (int i = 0; i < destBmp.Width; i++)
+            {
+                for (int j = 0; j < destBmp.Height; j++)
+                {
+                    double dx = 0;
+                    dx = bXDir ? (PI2 * (double)j) / dBaseAxisLen : (PI2 * (double)i) / dBaseAxisLen;
+                    dx += dPhase;
+                    double dy = Math.Sin(dx);
+
+                    // 取得当前点的颜色
+                    int nOldX = 0, nOldY = 0;
+                    nOldX = bXDir ? i + (int)(dy * dMultValue) : i;
+                    nOldY = bXDir ? j : j + (int)(dy * dMultValue);
+
+                    System.Drawing.Color color = srcBmp.GetPixel(i, j);
+                    if (nOldX >= 0 && nOldX < destBmp.Width
+                     && nOldY >= 0 && nOldY < destBmp.Height)
+                    {
+                        destBmp.SetPixel(nOldX, nOldY, color);
+                    }
+                }
+            }
+
+            return destBmp;
+        }
+
+        string code;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            code = GenerateCheckCode();//生成4位数字符串
+            Bitmap image = CreateCheckCodeImage(code, 64, 30);//生成图片
+            pictureBox11.Image=image;//给控件赋值
         }
     }
 }
